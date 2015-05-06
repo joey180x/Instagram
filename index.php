99a310d8819253a3c1abbcfe3e3f662dfe4fcbbe
Joseph De La Portilla <josephdlp18@gmail.com>
2015-05-06 08:16:28 -0700

    making sure github still works after re naming the project

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ set_time_limit(0);//no time limiit
 ini_set('default_socket_timeout', 300);//timeout
 session_start();//starting session
 
-//Make Constants using define.
+//Make Consta nts using define.
 define('client_id', '286f5d5cab964acea2333b38249165d2'); //insert client id);
 define('client_secret', '60bc0c4f4b3a4c3a97583145f696ede2'); //insert client secret);
 define('redirectURI', 'https://localhost:8888/appacademyapi/index.php'); //insert URI);
