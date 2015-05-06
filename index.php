0d71a812cecaaee6e63b9f5d1042a128e9412cbb
Joseph De La Portilla <josephdlp18@gmail.com>
2015-05-06 08:10:23 -0700

    Completed Milestone 3. Added basic HTML

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ define('client_secret', '60bc0c4f4b3a4c3a97583145f696ede2'); //insert client sec
 define('redirectURI', 'https://localhost:8888/appacademyapi/index.php'); //insert URI);
 define('ImageDirectory', 'pics/');
 ?>
-
 <!-- CLIENT INFO
 CLIENT ID 286f5d5cab964acea2333b38249165d2
 CLIENT SECRET 60bc0c4f4b3a4c3a97583145f696ede2
@@ -17,3 +16,19 @@ WEBSITE URL https://localhost:8888/appacademyapi/index.php
 REDIRECT URI https://localhost:8888/appacademyapi/index.php
 
 -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="decription" context="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Untitled</title>
+		<link rel="stylesheet" type="text/css" href="humans.txt">
+	</head>
+	<body>
+	<!-- Creating a login for people to go and give approval for our web app to sccess their Instagram Account
+	After getting approval we are now going to have the information and we can play with it. -->
+	<a href="https://api.instgram/oauth/authorize/?client_id=<?php echo client_ID; ?>&redirect_uri=<?php redirectURI; ?>&response_type=code">LOGIN</a>
+	<script type="text/javascript" src="js/main.js"></script>
+	</body>
+</html>
