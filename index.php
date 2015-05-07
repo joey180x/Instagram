1d34c95a52009270212d431dfdf426445c916271
Joseph De La Portilla <josephdlp18@gmail.com>
2015-05-06 21:28:34 -0700

    Completed milestone 5 with errors

--- index.php
+++ index.php
@@ -8,6 +8,25 @@ define('clientID', '286f5d5cab964acea2333b38249165d2'); //insert client id);
 define('clientSecret', '60bc0c4f4b3a4c3a97583145f696ede2'); //insert client secret);
 define('redirectURI', 'https://localhost/appacademyapi/index.php'); //insert URI);
 define('ImageDirectory', 'pics/');
+
+if isset(($_GET['code'])){
+	$code = ($_GET['code']);
+	//variable code is calling the code variable
+	$url = 'https://api.instagram.com/oauth/access_token';
+	//the url is the access token for instagram
+	$access_token_settings = array('client_id' => clientID,
+									//'clientID' if equal to the actual clientID
+									'cliend_secret' => clientSecret,
+									//'client secret' if equal to the actual client secret
+									'grant_type' = > 'authorization_code',
+									//'grant type is equal to the authorization code'
+									'redirect_uri' => redirectURI,
+									//'redirect_uri' if equal to the actual redirect uri
+									'code' => $code
+									//code is equal to the variable code
+									);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +36,8 @@ define('ImageDirectory', 'pics/');
 		<meta name="decription" context="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- <title>Untitled</title> -->
-		<!-- <link rel="stylesheet" type="text/css" href="humans.txt"> -->
+		<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+		<link rel="author" href="humans.txt">
 	</head>
 	<body>
 	<!-- Creating a login for people to go and give approval for our web app to sccess their Instagram Account
