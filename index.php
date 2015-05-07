3834eef9d5f4cfe3a64076556e4339fab862ca9d
Joseph De La Portilla <josephdlp18@gmail.com>
2015-05-06 21:53:08 -0700

    Finished milestone 7. Setting up curl

--- index.php
+++ index.php
@@ -25,8 +25,16 @@ if (isset($_GET['code'])){
 									'code' => $code
 									//code is equal to the variable code
 									);
-}
+//cURL is what we use in PHP, it's a library to other API's.
+$curl = curl_init($url);//stting a cURL session and we out in $url because that's where we are getting the data from
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings);//setting the POSTFIELDS to the array setup that we created
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //setting it equal to 1 because we are getting dtrings back.
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //but in live work-production we want to set this to true.
 
+}
+$result = curl_exec($curl);
+curl_close();
 ?>
 
 <!DOCTYPE html>
